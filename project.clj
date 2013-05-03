c3487edbad70b301077e5816e6532968925b4fdb
Aaron Craelius <aaroncraelius@gmail.com>
2013-05-03 14:59:15 -0400

    Removed brainstorming code.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject simple-schema "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Elegant functional validations and mutators for Clojure and Clojurescript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.monads "0.1.4"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
